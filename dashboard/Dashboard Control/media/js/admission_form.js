b3b3a17f09711d3ca7466906c29a74e53207faf4
Oasiz-admin <rider4585>
2023-06-06 09:09:10 +0530

    completed: admission form with backend

--- dashboard/Dashboard Control/media/js/admission_form.js
+++ dashboard/Dashboard Control/media/js/admission_form.js
@@ -13,7 +13,7 @@ class Form {
         this.sectionOneSubmitBtn = document.querySelector('.section-one-btn');
         this.sectionTwoSubmitBtn = document.querySelector('.section-two-btn');
         this.sectionThreeSubmitBtn = document.querySelector('.section-three-btn');
-        this.sectionOneElements = document.querySelectorAll('.section-one');
+        this.sectionOneElements = document.querySelector('.section-one');
         this.sectionTwoElements = document.querySelector('.section-two');
         this.sectionThreeElements = document.querySelector('.section-three');
 
@@ -32,9 +32,9 @@ class Form {
     attachEventListeners() {
         this.ageInput.addEventListener('click', () => this.calculateAge());
         this.otherBtn.addEventListener('click', () => this.toggleInputBox());
-        this.sectionOneSubmitBtn.addEventListener('click', () => this.showSectionOne());
-        this.sectionTwoSubmitBtn.addEventListener('click', () => this.showSectionTwo());
-        this.sectionThreeSubmitBtn.addEventListener('click', () => this.showSectionThree());
+        this.sectionOneSubmitBtn.addEventListener('click', () => this.showSectionTwo());
+        this.sectionTwoSubmitBtn.addEventListener('click', () => this.showSectionThree());
+        this.sectionThreeSubmitBtn.addEventListener('click', () => this.submitCompleteForm());
 
         this.buttons.forEach(button => {
             button.addEventListener('click', (event) => {
@@ -65,78 +65,127 @@ class Form {
         this.inputBoxContainer.style.display = this.otherBtn.classList.contains('selected') ? 'inline-block' : 'none';
     }
 
-    showSectionOne() {
+    showSectionTwo() {
 
         this.masterJSON.personalInfo = this.generateJSONForSectionOne();
-
-        for (let i = 0; i < this.sectionOneElements.length; i++) {
-            this.sectionOneElements[i].classList.add('hide');
-        }
-
+        this.sectionOneElements.classList.add('hide');
         this.sectionTwoElements.classList.remove('hide');
-        this.sectionTwoElements.classList.add('show');
-
-        console.log(this.masterJSON);
-    }
-
-    showSectionTwo() {
-        this.masterJSON.healthQuestions = this.generateJSONForSectionTwoA();
-        this.sectionTwoElements.classList.add('hide');
-        this.sectionTwoElements.classList.remove('show');
-        this.sectionThreeElements.classList.remove('hide');
-
-        console.log(this.masterJSON);
     }
 
     showSectionThree() {
+        this.sectionTwoElements.classList.remove('show');
+        this.sectionTwoElements.classList.add('hide');
+        this.sectionThreeElements.classList.remove('hide');
+        this.masterJSON.healthQuestions = this.generateJSONForSectionTwoA();
+        this.masterJSON.healthQuestions = this.generateJSONForSectionTwoB();
+        this.masterJSON.healthQuestions = this.generateJSONForSectionTwoC();
+        this.masterJSON.isConsentGiven = this.submitCompleteForm();
 
     }
 
     generateJSONForSectionOne() {
-        var formData = {};
-        formData.Date = document.getElementById("inputDate").value;
-        formData.ActivationDate = document.getElementById("inputActivationDate").value;
-        formData.FirstName = document.getElementById("inputFirstName").value;
-        formData.MiddleName = document.getElementById("inputMiddleName").value;
-        formData.LastName = document.getElementById("inputLastName").value;
-        formData.Address = document.getElementById("inputAddress").value;
-        formData.Mobile = document.getElementById("inputMobile").value;
-        formData.Email = document.getElementById("inputEmail").value;
-        formData.DOB = document.getElementById("inputDOB").value;
-        formData.Age = document.getElementById("inputAge").value;
-        formData.Gender = document.getElementById("inputGender").value;
-        formData.BloodGroup = document.getElementById("inputBloodGroup").value;
-        formData.Anniversary = document.getElementById("inputAnniversary").value;
+        let sectionOneFormData = {};
 
-        console.log(formData);
-        return formData;
+        sectionOneFormData.FirstName = document.getElementById("inputFirstName").value;
+        sectionOneFormData.MiddleName = document.getElementById("inputMiddleName").value;
+        sectionOneFormData.LastName = document.getElementById("inputLastName").value;
+        sectionOneFormData.Address = document.getElementById("inputAddress").value;
+        sectionOneFormData.Mobile = document.getElementById("inputMobile").value;
+        sectionOneFormData.Email = document.getElementById("inputEmail").value;
+        sectionOneFormData.DOB = document.getElementById("inputDOB").value;
+        sectionOneFormData.Age = document.getElementById("inputAge").value;
+        sectionOneFormData.Gender = document.getElementById("inputGender").value;
+        sectionOneFormData.BloodGroup = document.getElementById("inputBloodGroup").value;
+        sectionOneFormData.Anniversary = document.getElementById("inputAnniversary").value;
+
+        this.masterJSON.date = document.getElementById("inputDate").value;
+        this.masterJSON.activationDate = document.getElementById("inputActivationDate").value;
+
+        return sectionOneFormData;
     }
 
     generateJSONForSectionTwoA() {
-        // isAllSectionOneAFilled = false;
-        var questions = document.querySelectorAll('.question-container li');
-
-        var formAnswers = {
-            questions: []
+        let jsonData = {
+            "question-section-1": {
+                "question-1": document.querySelector('input[name="section-one-question-1"]:checked').value,
+                "question-2": document.querySelector('input[name="section-one-question-2"]:checked').value,
+                "question-3": document.querySelector('input[name="section-one-question-3"]:checked').value,
+                "question-4": document.querySelector('input[name="section-one-question-4"]:checked').value,
+                "question-5": document.querySelector('input[name="section-one-question-5"]:checked').value,
+                "question-6": document.querySelector('input[name="section-one-question-6"]:checked').value,
+                "question-7": document.querySelector('input[name="section-one-question-7"]:checked').value
+            }
         };
 
-        questions.forEach(function (question) {
-            console.log(question);
+        return jsonData;
+    }
 
-            if (question.querySelector('input[type="radio"]:checked')) {
-                var questionId = question.querySelector('input[type="radio"]').name;
-                var answer = question.querySelector('input[type="radio"]:checked');
-
-                if (answer.value !== null | answer.value !== "") {
-                    formAnswers.questions.push({
-                        id: questionId,
-                        answer: answer.value
-                    });
-                }
+    generateJSONForSectionTwoB() {
+        let jsonData = {
+            "question-section-2": {
+                "question-1": document.querySelector('input[name="section-two-question-1"]:checked').value,
+                "question-2": document.querySelector('input[name="section-two-question-2"]').value,
+                "question-3": document.querySelector('input[name="section-two-question-3"]').value,
+                "question-4": document.querySelector('input[name="section-two-question-4"]').value,
+                "question-5": document.querySelector('input[name="section-two-question-5"]:checked').value,
+                "question-6": document.querySelector('input[name="section-two-question-6"]').value,
+                "question-7": document.querySelector('input[name="section-two-question-7"]').value,
+                "emergency_contact_name": document.querySelector('input[name="emergency_contact_name"]').value,
+                "emergency_contact_number": document.querySelector('input[name="emergency_contact_number"]').value,
+                "question-9": document.querySelector('input[name="section-two-question-8"]:checked').value,
+                "question-10": document.querySelector('input[name="section-two-question-9"]').value,
+                "question-11": document.querySelector('input[name="section-two-question-10"]').value,
+                "question-12": document.querySelector('input[name="section-two-question-11"]:checked').value,
+                "question-13": document.querySelector('input[name="section-two-question-12"]:checked').value
             }
-        });
-        // console.log(formAnswers);
-        // return formAnswers;
+        };
+        return jsonData;
+    }
+
+    generateJSONForSectionTwoC() {
+        const buttons = document.querySelectorAll('.medical-condition-btn');
+        const otherInput = document.querySelector('#otherInput');
+        const conditions = [];
+
+        for (const element of buttons) {
+            if (element.classList.contains('selected')) {
+                conditions.push(element.textContent);
+            }
+        }
+
+        const otherConditionsInputValue = otherInput.value.trim();
+        if (otherConditionsInputValue !== '') {
+            const otherConditions = otherConditionsInputValue.split(',').map(condition => condition.trim());
+            conditions.push(...otherConditions);
+        }
+
+        const jsonData = {
+            medicalConditions: conditions
+        };
+
+        return jsonData;
+    }
+
+    submitCompleteForm() {
+        const consentCheckbox = document.getElementById('consent-checkbox');
+        const consentGiven = consentCheckbox.checked;
+
+        if (!consentGiven) {
+            consentCheckbox.focus();
+            consentCheckbox.style.textShadow = "text-shadow: 0px 0px 15px rgba(232,72,72,0.85)";
+        } else {
+            const jsonData = {
+                consentGiven: consentGiven
+            };
+            return jsonData;
+        }
+    }
+
+    readDataFromFlutter(fName, mName, lName, mobileNo) {
+        document.getElementById("inputFirstName").value = fName;
+        document.getElementById("inputMiddleName").value = mName;
+        document.getElementById("inputLastName").value = lName;
+        document.getElementById("inputMobile").value = mobileNo;
     }
 }
 
